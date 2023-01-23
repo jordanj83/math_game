b11833ddfdf18e8a4f29fb5b1024d368446d410b
jordanj83 <jordan.johnson@telus.com>
2023-01-23 12:37:53 -0800

    added losing condition and statements

--- main.rb
+++ main.rb
@@ -10,20 +10,19 @@ count = 0
 until player1.lives == 0 && player2.lives == 0
    if player == 1
     if count != 0
-      puts '----- NEW TURN -----'
     end
     question = Question.new
     puts "player 1: #{question.question}"
-    print ">"
+    print ">>"
     answer = gets.chomp.to_i
     if answer == question.answer
-      puts 'Player 1: YES! You are correct.'
+      puts 'Player 1: Good job!.'
       puts "P1: #{player1.lives}/3 vs P2: #{player2.lives}/3"
       player = 2
       count +=1
     else
       player1.lives -=1
-      puts 'Player 1: Seriously? No!'
+      puts 'Player 1: loser'
       puts "P1: #{player1.lives}/3 vs P2: #{player2.lives}/3"
       if player1.lives == 0
         break
@@ -34,6 +33,45 @@ until player1.lives == 0 && player2.lives == 0
 
   end
 
+  if player == 2
+    if count != 0
+    end
+    question = Question.new
+    puts "player 2: #{question.question}"
+    print ">>"
+    answer = gets.chomp.to_i
+    if answer == question.answer
+      puts 'No gong for you!'
+      puts "P1: #{player1.lives}/3 vs P2: #{player2.lives}/3"
+      player = 1
+      count +=1
+    else
+      player2.lives -=1
+      puts 'Player 2: no whammy, no whammy, no whammy'
+      puts "P1: #{player1.lives}/3 vs P2: #{player2.lives}/3"
+      if player2.lives == 0
+        break
+      end
+      player = 1
+      count +=1
+    end
+
+
+  end
+
+end
+
+if (player1.lives == 0)
+  puts "You are the lizard queen! #{player2.lives}/3"
+  puts '----- GAME OVER -----'
+  puts 'Get lost!'
+end
+if(player2.lives == 0)
+  puts "Welcome to the planet of the apes #{player1.lives}/3"
+  puts '----- GAME OVER -----'
+  puts 'Sorry lady, shows over'
+
+end
 
 
 
--- question.rb
+++ question.rb
@@ -4,7 +4,7 @@ class Question
   def initialize
     #initialize two numbers between 1 and 20 
     @number1 = rand(1..20)
-    @number2 =rand(1..20)
+    @number2 = rand(1..20)
     @answer = @number1 + @number2
     @question = "What is #{@number1} + #{@number2} = ?"
   end
