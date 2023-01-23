f6cfd2c7cf79244875ca2406675029ce5761b9a2
jordanj83 <jordan.johnson@telus.com>
2023-01-23 12:24:25 -0800

    player one and two question caller

--- main.rb
+++ main.rb
@@ -1,6 +1,5 @@
-require "./players.rb"
-require "./question.rb"
-require "./main.rb"
+require "./players"
+require "./question"
 
 player1 = Player.new
 player2 = Player.new
@@ -8,7 +7,33 @@ player2 = Player.new
 player = 1
 count = 0
 
-#Start off the game with both players alive and 1st player
+until player1.lives == 0 && player2.lives == 0
+   if player == 1
+    if count != 0
+      puts '----- NEW TURN -----'
+    end
+    question = Question.new
+    puts "player 1: #{question.question}"
+    print ">"
+    answer = gets.chomp.to_i
+    if answer == question.answer
+      puts 'Player 1: YES! You are correct.'
+      puts "P1: #{player1.lives}/3 vs P2: #{player2.lives}/3"
+      player = 2
+      count +=1
+    else
+      player1.lives -=1
+      puts 'Player 1: Seriously? No!'
+      puts "P1: #{player1.lives}/3 vs P2: #{player2.lives}/3"
+      if player1.lives == 0
+        break
+      end
+      player = 2
+      count +=1
+    end
+
+  end
+
 
 
 
--- question.rb
+++ question.rb
@@ -1,12 +1,12 @@
 class Question
-attr_accessor :answer
-  def initialize(num1, num2)
-    @num1 = rand(1..20)
-    @num2 = rand(1..20)
-    @answer = @num1 + @num2
-    @question = "What does #{@num1} '+' #{@num2} equal?"
+  attr_accessor :question, :answer
+
+  def initialize
+    #initialize two numbers between 1 and 20 
+    @number1 = rand(1..20)
+    @number2 =rand(1..20)
+    @answer = @number1 + @number2
+    @question = "What is #{@number1} + #{@number2} = ?"
   end
 
 end
-
-
